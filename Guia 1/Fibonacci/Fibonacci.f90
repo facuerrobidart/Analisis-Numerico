34277a778fb75cf706828f242aa4d2ec39d5945a
Facundo Errobidart <facundoerrobidart2001@gmail.com>
2021-08-30 16:43:47 -0300

    plot fibonacci

--- Guia 1/Fibonacci/Fibonacci.f90
+++ Guia 1/Fibonacci/Fibonacci.f90
@@ -6,8 +6,8 @@ INTEGER termino
 
 WRITE (*,*) 'Ingrese el termino de fibonacci a calcular'
 READ *, termino
-
-WRITE (*,*) fibo(termino)
+CALL hacer(termino)
+CALL graficar
 
 CONTAINS
 
@@ -23,5 +23,35 @@ suma = fibo(termino-1)+fibo(termino-2)
 END IF
 END FUNCTION
 
+SUBROUTINE hacer(termino)
+INTEGER termino,i
 
+OPEN(2,FILE='fibo.dat',STATUS='REPLACE')
+
+DO i=0,termino
+WRITE (*,'(2I8)') i,fibo(i)
+WRITE (2,'(2I4)') i,fibo(i)
+END DO
+
+CLOSE(2,STATUS='KEEP')
+END SUBROUTINE
+
+
+SUBROUTINE graficar
+
+OPEN(3,FILE='script.p',STATUS='REPLACE')
+WRITE(3,*)'set autoscale'
+WRITE(3,*) 'unset log'
+WRITE(3,*) 'unset label'
+WRITE(3,*) 'set xtic auto'
+WRITE(3,*) 'set ytic auto'
+WRITE(3,*) 'set title "Sucesion de Fibonacci"'
+WRITE(3,*) 'set xlabel "termino"'
+WRITE(3,*) 'set ylabel "valor"'
+WRITE(3,*) 'plot "fibo.dat" with lines'
+
+CLOSE(3,STATUS='KEEP')
+
+CALL SYSTEM("C:\gnuplot\bin\gnuplot.exe -persist script.p")
+END SUBROUTINE
 END PROGRAM
---
+++ Guia 1/Fibonacci/script.p
@@ -0,0 +1,9 @@
+ set autoscale
+ unset log
+ unset label
+ set xtic auto
+ set ytic auto
+ set title "Sucesion de Fibonacci"
+ set xlabel "termino"
+ set ylabel "valor"
+ plot "fibo.dat" with lines
